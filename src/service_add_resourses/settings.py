e0f921096cfd074b68222f20c2a53108fa285f99
ArtemGrom <art.gromov@bk.ru>
2022-07-07 15:02:08 +0300

    create sign_up, login, logged out

--- src/service_add_resourses/settings.py
+++ src/service_add_resourses/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+#don't use that on prodaction
+SECRET_KEY = 'django-insecure-f6ldi+vr07l+5dgl^503_=h6ak2ajd*-7s$_e8af4t9hbd_^4d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
